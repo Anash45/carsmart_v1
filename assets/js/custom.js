4159a2025459eb70946aff0d2c9057a35f6a98c9
Syed Anas Bukhari <anas14529@gmail.com>
2024-11-18 09:40:14 +0500

    New pages added.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,10 +34,10 @@ $(document).ready(function () {
         const $input = $(this).closest('.fpf-inp-cont').find('.fpf-inp');
         const $input_cont = $(this).closest('.fpf-inp-cont');
         $icon.on('click', function () {
-            if($input.attr('type') === 'password') {
+            if ($input.attr('type') === 'password') {
                 $input.attr('type', 'text');
                 $icon.addClass('shown');
-            }else {
+            } else {
                 $input.attr('type', 'password');
                 $icon.removeClass('shown');
             }
@@ -51,35 +51,83 @@ $(document).ready(function () {
     //     });
     // });
 
-    $(document).ready(function () {
-        // Function to add a class to the active tab's parent .nav-item
-        function updateActiveSignupTab() {
-            // Remove the custom class from all nav-items
-            $('.sfs-nav .nav-item').removeClass('active-tab');
+});
 
-            // Find the active tab
-            const activeTab = $('.sfs-nav .nav-link.active');
+$(document).ready(function () {
+    // Function to add a class to the active tab's parent .nav-item
+    function updateActiveSignupTab() {
+        // Remove the custom class from all nav-items
+        $('.sfs-nav .nav-item').removeClass('active-tab');
 
-            if (activeTab.length) {
-                // Add the class to the parent .nav-item of the active tab
-                activeTab.closest('.nav-item').addClass('active-tab');
-            }
+        // Find the active tab
+        const activeTab = $('.sfs-nav .nav-link.active');
+
+        if (activeTab.length) {
+            // Add the class to the parent .nav-item of the active tab
+            activeTab.closest('.nav-item').addClass('active-tab');
         }
+    }
 
-        // Call the function on page load
+    // Call the function on page load
+    updateActiveSignupTab();
+
+    // Call the function on tab change
+    $('.sfs-nav .nav-link').on('shown.bs.tab', function () {
         updateActiveSignupTab();
-
-        // Call the function on tab change
-        $('.sfs-nav .nav-link').on('shown.bs.tab', function () {
-            updateActiveSignupTab();
-        });
     });
 
+    if ($('.ts-slider-cont').length > 0) {
+        $('.ts-slider-cont').slick({
+            centerMode: true,
+            centerPadding: '400px', /* Adjust this to control how much of the side items are visible */
+            slidesToShow: 1,
+            focusOnSelect: true,
+            autoplay: true, // Autoplay enabled
+            autoplaySpeed: 2000, // Autoplay speed in milliseconds
+            prevArrow: `<button type="button" class="slick-arrows slick-prev">
+    <svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="0.5" width="47" height="47" rx="23.5" stroke="#101044"/>
+<path d="M32.5 23H20.33L25.92 17.41L24.5 16L16.5 24L24.5 32L25.91 30.59L20.33 25H32.5V23Z" fill="#101044"/>
+</svg>
+
+    </button>`,
+            nextArrow: `<button type="button" class="slick-arrows slick-next">
+    <svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="0.5" width="47" height="47" rx="23.5" stroke="#101044"/>
+<path d="M24.5 16L23.09 17.41L28.67 23H16.5V25H28.67L23.09 30.59L24.5 32L32.5 24L24.5 16Z" fill="#101044"/>
+</svg>
+
+    </button>`,
+    appendArrows: '.ts-slider-arrows',
+            dots: false, // Hide navigation dots
+            draggable: true, // Enable dragging
+            infinite: true, // Infinite looping
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 1,
+                        centerPadding: '200px',
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1,
+                        centerMode: false,
+                        centerPadding: '0px',
+                    }
+                }
+            ]
+        });
+    }
+
+    AOS.init();
 });
 
 function switchTab(tabId) {
     // Use Bootstrap's tab method to show the specified tab
-    var tab = new bootstrap.Tab(document.getElementById(tabId+'-tab'));
+    var tab = new bootstrap.Tab(document.getElementById(tabId + '-tab'));
     tab.show(); // This will activate the tab
 }
 
